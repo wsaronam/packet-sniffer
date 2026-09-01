51b3616f2737d5cf6700390d8b56985186917abc
Willy Saronamihardja <wsaronam@gmail.com>
2026-09-01 00:26:50 -0700

    Update main.py
    - working on building packetsniffer main (still WIP)

--- src/packet_sniffer/cli/main.py
+++ src/packet_sniffer/cli/main.py
@@ -1,4 +1,7 @@
 import sys
+import argparse
+
+from scapy.all import get_if_list
 
 from packet_sniffer.core.packet_model import Packet
 from packet_sniffer.core.sniffer import PacketSniffer
@@ -11,13 +14,57 @@ def make_packet_handler(storage: PacketStorage):
     '''
     returns callback function that saves the packet to storage
     '''
-    return
+    def handle_packet(packet: Packet) -> None:
+        pak = packet.timestamp.strftime('%H:%M:%S.%f')[:-3]
+        print(f'[{pak}] {packet.summary} ({packet.length} bytes)')
+        storage.save(packet)
+    return handle_packet
+
+
+def build_arg_parser() -> argparse.ArgumentParser:
+    parser = argparse.ArgumentParser(
+        prog='packet-sniffer',
+        description='network packet sniffer using Scapy'
+    )
+    parser.add_argument(
+        '-i', '--interface',
+        help='Network interface to capture on (ex "Wi-Fi" or "Ethernet")'
+            'Defaults to Scapy default interface',
+        default=None
+    )
+
+    return parser
+
+
+def print_interfaces() -> None:
+    '''
+    prints available interfaces for user to pick
+    '''
+    print("Available interfaces:")
+    for iface in get_if_list():
+        print(f' - {iface}')
 
 
 
 def main() -> None:
-    #parser = build_parser()
-    storage = PacketStorage()
+    parser = build_arg_parser()
+    args = parser.parse_args()
+
+    if args.print_interfaces:
+        print_interfaces()
+        return
+
+    storage = PacketStorage(args.db)
+
+    if args.clear_db:
+        storage.clear()
+        print(f'Cleared all packets from {args.db}')
+        return
+
+
+    print('Packet Sniffer - press Ctrl+C to stop')
+    print(f'Saving captures to: {args.db}')
+
     sniffer = PacketSniffer(
         callback='',
         interface='',
@@ -27,10 +74,13 @@ def main() -> None:
 
     try:
         sniffer.start()
+    except PermissionError:
+        print('Permission denied.  Please make sure your have proper administrator permissions.',
+              file=sys.stderr
+        )
+        sys.exit(1)
     except KeyboardInterrupt:
         pass
-    except:
-        sys.exit(1)
     finally:
         total = storage.total_count()
         print(f'Capture stopped.  {total} packet(s) total')
